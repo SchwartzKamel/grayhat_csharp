54f5d1346dd3f83730b3e1f83cd5cda7bb976083
SchwartzKamel <tommy.weber123@gmail.com>
2024-12-29 16:05:51 -0800

    Update Makefile, auto remove container on exit, add comments for Platform Invoke C# file

--- Makefile
+++ Makefile
@@ -1,8 +1,22 @@
 build_ch1:
 	docker build -t sierrahackingco/grayhat_csharp_ch1:latest -f ch1/Dockerfile .
 
+run_ch1:
+	docker run -it --rm --name grayhat_csharp_ch1 sierrahackingco/grayhat_csharp_ch1:latest
+
 test_ch1:
-	docker run -it --name grayhat_csharp_ch1 sierrahackingco/grayhat_csharp_ch1:latest
+	make build_ch1
+	make run_ch1
+
+build_ch2:
+	docker build -t sierrahackingco/grayhat_csharp_ch2:latest -f ch2/Dockerfile .
+
+run_ch2:
+	docker run -it --rm --name grayhat_csharp_ch2 sierrahackingco/grayhat_csharp_ch2:latest
+
+test_ch2:
+	make build_ch2
+	make run_ch2
 
 help:
-	@echo "build_ch1 test_ch1"
+	@echo " test_ch1 - Build and deploy Chapter 1\n test_ch2 - Build and deploy Chapter 2"
--- ch1/pinvoke/Main.cs
+++ ch1/pinvoke/Main.cs
@@ -5,23 +5,27 @@ namespace ch1_pinvoke
 {
 	class MainClass
 	{
+		// DLLImport for Win32NT native call
 		[DllImport("user32", CharSet=CharSet.Auto)]
 		static extern int MessageBox(IntPtr hWnd, 
 			String text, String caption, int options);
 
+		// DLLImport for C `printf` statement
 		[DllImport("libc")]
 		static extern void printf(string message);
 
 		static void Main(string[] args)
 		{
+			// Grab OS and have branching native calls for Win32NT or other (*nix)
 			OperatingSystem os = Environment.OSVersion;
 
 			if (os.Platform == PlatformID.Win32Windows 
 				|| os.Platform == PlatformID.Win32NT) {
-
+				// If Windows, use native call to have a popup window appear
 				MessageBox (IntPtr.Zero, "Hello world!", "Hello world!", 0);
 			} else {
-				printf ("Hello world!");
+				// Print to console
+				printf ("Hello world!\n");
 			}
 		}
 	}
